a89f4f0f2a14b0c0a19769081f830e53fcb77f41
hardy lutula <dylut@hardys-MacBook-Air.local>
2019-10-23 21:45:51 +0200

    good and simple app using telegram

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 require('dotenv').config();
 
 const TelegramBot = require('node-telegram-bot-api');
@@ -8,6 +10,12 @@ const bot = new TelegramBot(token, {
     polling: true
 });
 
+// In-memory storage
+const URLs = [];
+const URLLabels = [];
+let tempSiteURL = '';
+
+
 // Listener (handler) for telegram's /bookmark event
 bot.onText(/\/bookmark/, (msg, match) => {
     const chatId = msg.chat.id;
@@ -15,7 +23,7 @@ bot.onText(/\/bookmark/, (msg, match) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
     // of the message
- 
+
     if (url === undefined) {
         bot.sendMessage(
             chatId,
@@ -23,10 +31,177 @@ bot.onText(/\/bookmark/, (msg, match) => {
         );
         return;
     }
- 
+
     URLs.push(url);
     bot.sendMessage(
         chatId,
         'URL has been successfully saved!',
+    );
+});
+
+
+// We have added a third parameter to our bot.sendMessage function. This object defines the options 
+// for our keyboard. If you take a closer look at reply_markup.keyboard, you find an array that contains 
+// more arrays. Every array here represents a row with options. This helps you with formatting options 
+// that are more important or have longer button text.
+
+// Listener (handler) for showcasing different keyboard layout
+// normal keyboard
+bot.onText(/\/keyboard/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'Alternative keybaord layout', {
+        'reply_markup': {
+            'keyboard': [['Sample text', 'Second sample'], ['Keyboard'], ['I\'m robot']],
+            resize_keyboard: true,
+            one_time_keyboard: true,
+            force_reply: true,
+        }
+    });
+});
+
+
+// The reply_markup now defines the property inline_keyboard. The rest of the markup remains the same. 
+// Every array represents a row. However, instead of just passing text, we can also pass an object with 
+// text and callback_data to know what option you have selected.
+
+// Listener (handler) for telegram's /label event
+// inline keyboard
+bot.onText(/\/label/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const url = match.input.split(' ')[1];
+
+    if (url === undefined) {
+        bot.sendMessage(
+            chatId,
+            'Please provide URL of article!',
+        );
+        return;
+    }
+
+    tempSiteURL = url;
+    bot.sendMessage(
+        chatId,
+        'URL has been successfully saved!',
+        {
+            reply_markup: {
+                inline_keyboard: [[
+                    {
+                        text: 'Development',
+                        callback_data: 'development'
+                    }, {
+                        text: 'Lifestyle',
+                        callback_data: 'lifestyle'
+                    }, {
+                        text: 'Other',
+                        callback_data: 'other'
+                    }
+                ]]
+            }
+        }
+    );
+});
+
+
+// In case we want to receive callback data, we have to listen to this type of event.
+// Listener (handler) for callback data from /label command
+
+
+// Listener (handler) for callback data from /label command
+bot.on('callback_query', (callbackQuery) => {
+    const message = callbackQuery.message;
+    const category = callbackQuery.data;
+
+    console.log(callbackQuery.data);
+
+    URLLabels.push({
+        url: tempSiteURL,
+        label: category,
+    });
+
+    tempSiteURL = '';
+
+    bot.sendMessage(message.chat.id, `URL has been labeled with category "${category}"`);
+});
+
+
+
+// Inline keyboard options
+const inlineKeyboard = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                {
+                    text: 'YES',
+                    callback_data: JSON.stringify({
+                        'command': 'mycommand1',
+                        'answer': 'YES'
+                    })
+                },
+                {
+                    text: 'NO',
+                    callback_data: JSON.stringify({
+                        'command': 'mycommand1',
+                        'answer': 'NO'
+                    })
+                },
+            ]
+        ]
+    }
+};
+
+
+
+// Keyboard layout for requesting phone number access
+const requestPhoneKeyboard = {
+    "reply_markup": {
+        "one_time_keyboard": true,
+        "keyboard": [[{
+            text: "My phone number",
+            request_contact: true,
+            one_time_keyboard: true
+        }], ["Cancel"]]
+    }
+ };
+
+
+ // Listener (handler) for retrieving phone number
+bot.onText(/\/phone/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'Can we get access to your phone number?', requestPhoneKeyboard);
+ 
+ });
+
+
+ // Handler for phone number request when user gives permission
+bot.on('contact', async (msg) => {
+    const phone = msg.contact.phone_number;
+    bot.sendMessage(msg.chat.id, `Phone number saved: ${phone}`);
+})
+
+ bot.on('message', (msg) => {
+    var location = "location";
+    console.log('location');
+    if (msg.text.indexOf(location) === 0) {
+        bot.sendLocation(msg.chat.id,44.97108, -104.27719);
+        bot.sendMessage(msg.chat.id, "Here is the point");
+
+    }
+});
+
+
+// Listener (handler) for telegram's /start event
+// This event happened when you start the conversation with both by the very first time
+// Provide the list of available commands
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+        chatId,
+        `
+            Welcome at <b>ArticleBot</b>, thank you for using my service
+      
+            Available commands:
+        
+            /bookmark <b>URL</b> - save interesting article URL
+        `, {
+            parse_mode: 'HTML',
+        }
     );
 });
